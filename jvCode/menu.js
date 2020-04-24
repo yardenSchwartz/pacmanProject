6e8ad805f3bd43f5884d097545eefaac4bf53cc5
yarden <yardensch95@gmail.com>
2020-04-24 13:19:45 +0300

    opend folders

---
+++ jvCode/menu.js
@@ -0,0 +1,10 @@
+
+function changeWindow(id) {
+    let section = document.getElementsByTagName('section');
+
+    for(i=0; i<section.length; i++){
+            section[i].style.display = 'none';
+    }
+    
+    document.getElementById(id.getAttribute("data-option")).style.display = "block"
+}
